625f745fbe806fd5ee2e374d5671aa2fc209101f
Daya <fedosienkodiana14@gmail.com>
2025-02-07 15:13:17 +0200

    update file

--- src/js/dollar.js
+++ src/js/dollar.js
@@ -1,19 +1,19 @@
 export function dollar() { 
 document.querySelector('.paragraph').addEventListener('click', function(event) {
-    for (let i = 0; i < 10; i++) {
-        let dollar = document.createElement('img');
-        dollar.src = './img/dollar.png'; 
-        dollar.classList.add('flying-dollar');
-        document.body.appendChild(dollar);
+    for (let i = 0; i < 1; i++) {
+        let flyingDollar = document.createElement('img');
+        flyingDollar.src = './img/dollar.png'; 
+        flyingDollar.classList.add('flying-dollar');
+        document.body.appendChild(flyingDollar);
 
          const rect = this.getBoundingClientRect(); 
   flyingDollar.style.top = `${rect.top - 50}px`; 
-  flyingDollar.style.left = `${rect.left + Math.random() * rect.width - 50}px`;
+  flyingDollar.style.left = `${rect.left + Math.random() * 100 - 50}px`;
 
 
         setTimeout(() => {
-            dollar.remove();
-        }, 1500);
+            flyingDollar.remove();
+        }, 5000);
     }
 });
 

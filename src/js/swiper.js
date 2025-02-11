3a8711c4473eb19f7a67a2c0755757bb6b20659e
Daya <fedosienkodiana14@gmail.com>
2025-02-11 22:20:21 +0200

    update swiper

---
+++ src/js/swiper.js
@@ -0,0 +1,74 @@
+import Swiper from 'swiper';
+import { Navigation, Keyboard } from 'swiper/modules'; 
+
+export function initSwiper () { 
+    const swiperAbout = new Swiper('.about-swiper', {
+    modules: [Navigation, Keyboard],
+    loop: false, 
+        slidesPerView: 1,
+    navigation: {
+        nextEl: '.about-next',
+        prevEl: '.about-prev',
+    },
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+    },
+        grabCursor: true,
+        breakpoints: { 
+            1440: { 
+                slidesPerView: 3,
+                 spaceBetween: 20,
+        }
+    },
+    on: {
+        init: function (swiper) {
+            updateButtons(swiper, '.about-prev', '.about-next');
+        },
+        slideChange: function (swiper) {
+            updateButtons(swiper, '.about-prev', '.about-next');
+        }
+    }
+    });
+
+    const swiperPhoto = new Swiper('.swiper-photo', {
+        modules: [Navigation, Keyboard],
+        loop: false, 
+        slidesPerView: 1,
+        navigation: {
+            nextEl: '.photo-prev',
+            prevEl: '.photo-next',
+        },
+        keyboard: {
+            enabled: true,
+            onlyInViewport: true,
+        },
+        grabCursor: true,
+             
+        on: {
+            init: function (swiper) {
+                updateButtons(swiper, '.photo-prev', '.photo-next');
+            },
+            slideChange: function (swiper) {
+                updateButtons(swiper, '.photo-prev', '.photo-next');
+            }
+        }
+    });
+    
+
+function updateButtons(swiper, prevSelector, nextSelector) {
+    const prevButton = document.querySelector(prevSelector);
+    const nextButton = document.querySelector(nextSelector);
+
+     if (!prevButton || !nextButton) return;
+  
+    prevButton.disabled = swiper.isBeginning;
+    nextButton.disabled = swiper.isEnd; 
+
+    prevButton.classList.toggle('swiper-button-disabled', swiper.isBeginning);
+    nextButton.classList.toggle('swiper-button-disabled', swiper.isEnd);
+    
+}
+}
+
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { setupMobileMenu } from "./js/mobile.js";
 import { scrollPhoto } from "./js/scroll.js";
 import { dollar } from "./js/dollar.js";
+import { initSwiper } from "./js/swiper.js";
 
 scrollPhoto();
 setupMobileMenu();
 dollar();
+initSwiper();
 

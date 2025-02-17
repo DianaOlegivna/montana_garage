71a9c92393f4e787013dcdc56883943d5b742f8c
Daya <fedosienkodiana14@gmail.com>
2025-02-17 15:45:18 +0200

    update

--- src/js/paralaks.js
+++ src/js/paralaks.js
@@ -1,31 +1,11 @@
 export function paralaks() { 
   document.addEventListener("DOMContentLoaded", () => {
-    // Ініціалізація загального паралаксу для всіх елементів з класом .rellax
     new Rellax('.rellax', {
-      speed: -2, // базова швидкість
+      speed: -2,
       center: true,
       round: true,
       vertical: true,
       horizontal: false
     });
-
-    // Ініціалізація для конкретних елементів з індивідуальними налаштуваннями
-    const rellaxIcons = new Rellax('.icons8-mercedes-benz', {
-      speed: 1, // налаштування для цього елементу
-      center: false,
-      round: true,
-      vertical: true,
-      horizontal: false
-    });
-
-    const rellaxAudi = new Rellax('.icons8-audi', {
-      speed: 3, // інший параметр
-      center: false,
-      round: true,
-      vertical: true,
-      horizontal: false
-    });
-
-    // Можна додавати ще інші налаштування для різних елементів за необхідності
   });
 }
--- src/main.js
+++ src/main.js
@@ -14,3 +14,6 @@ initSwiper();
 paralaks();
 simple();
 
+
+
+
